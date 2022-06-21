0ac97751b12564113fdfd51c9f34036cd6a91d22
lucas <54567493+lucas-alves-souza@users.noreply.github.com>
2022-06-21 20:43:52 -0300

    update eudation and experience

--- assets/js/app.js
+++ assets/js/app.js
@@ -120,8 +120,8 @@ let footer = $(`
       <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1.5em" height="1.5em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M7 21a4 4 0 0 1-4-4c0-1.473 1.333-6.14 4-14h2L7 13a4 4 0 1 1 0 8zm10 0a4 4 0 0 1-4-4c0-1.473 1.333-6.14 4-14h2l-2 10a4 4 0 1 1 0 8z" fill="#fff" fill-rule="evenodd"/></svg>
       </div>
       <p class="tag" >
-“Somewhere, something incredible is waiting to be known.”
-<br> _ Carl Sagan </p>
+“Two things awe me most, the starry sky above me and the moral law within me.”
+<br> _ Immanuel Kant </p>
     </div>
 
     
